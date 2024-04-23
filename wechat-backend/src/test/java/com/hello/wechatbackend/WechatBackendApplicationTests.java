3157b5dbe9fc55cb76b0b334d3add4a2be0ddc73
wp001 <1193558442@qq.com>
2024-04-23 20:31:32 +0800

    feat: 创建项目

---
+++ wechat-backend/src/test/java/com/hello/wechatbackend/WechatBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hello.wechatbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WechatBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
